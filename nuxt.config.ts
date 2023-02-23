832357f80583f269c0647e3b16b3867c5eb7ca81
eddieciou <a589658gur@gmail.com>
2023-02-23 13:29:03 +0800

    add tailwindcss

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    typescript: {
-        typeCheck: true
-    },
+  modules: ['@nuxtjs/tailwindcss'],
+  typescript: {
+    typeCheck: true
+  }
 })
