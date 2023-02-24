676177ded2e06f61737f49c4a7ea0506e6e44554
eddieciou <a589658gur@gmail.com>
2023-02-24 16:45:05 +0800

    layout practice

--- app.vue
+++ app.vue
@@ -1,5 +1,8 @@
 <template>
-  <div>
-    <NuxtPage />
+  <div class="m-4 bg-white">
+    <p class="pb-4 text-2xl text-slate-600">這裡是最外層 app.vue</p>
+    <NuxtLayout name="default">
+      <NuxtPage />
+    </NuxtLayout>
   </div>
 </template>
