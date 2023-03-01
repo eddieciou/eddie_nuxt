d9b25457e4a54e49aac482f63d45d38d90c13cf9
eddieciou <a589658gur@gmail.com>
2023-03-01 11:42:18 +0800

    實作延遲載入

--- app.vue
+++ app.vue
@@ -1,20 +1,13 @@
 <template>
   <div class="flex flex-col items-center">
     <div class="mt-8 flex items-center">
-      <input
-        id="show-button"
-        v-model="useRound"
-        name="show-button"
-        type="checkbox"
-        class="w5 h-5"
-      />
-      <label for="show-button" class="ml-2 block text-base text-slate-800">使用圓角按鈕</label>
+      <input id="show-button" v-model="show" name="show-button" type="checkbox" class="h-5 w-5" />
+      <label for="show-button" class="ml-2 block text-base text-slate-800">顯示報名按鈕</label>
     </div>
-    <component :is="useRound ? RoundButton : BaseButton" />
+    <LazyBaseApplyButton v-if="show" />
   </div>
 </template>
-<script setup lang="ts">
-const useRound = ref(false)
-const BaseButton = resolveComponent('BaseApplyButton')
-const RoundButton = resolveComponent('RoundApplyButton')
+
+<script setup>
+const show = ref(false)
 </script>
