3e102dee29a2b9aa6085ac1e7158ebc1ad28fa30
eddieciou <a589658gur@gmail.com>
2023-03-01 13:27:57 +0800

    實作客戶端渲染

--- app.vue
+++ app.vue
@@ -1,13 +1,10 @@
 <template>
-  <div class="flex flex-col items-center">
-    <div class="mt-8 flex items-center">
-      <input id="show-button" v-model="show" name="show-button" type="checkbox" class="h-5 w-5" />
-      <label for="show-button" class="ml-2 block text-base text-slate-800">顯示報名按鈕</label>
-    </div>
-    <LazyBaseApplyButton v-if="show" />
+  <div>
+    <ClientOnly>
+      <IronManWelcome />
+      <template #fallback>
+        <p class="my-6 flex justify-center">[IronManWelcome] 載入中...</p>
+      </template>
+    </ClientOnly>
   </div>
 </template>
-
-<script setup>
-const show = ref(false)
-</script>
